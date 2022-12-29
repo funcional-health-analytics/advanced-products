bbb1a053dfebb1d87aa1e7108910f59065c4ecb3
afflorezr2 <afflorezr2@github.com>
2022-12-29 14:51:46 -0300

    MBA working with snowflake data 2/8)

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM python:3.7-slim
 
-
 COPY . app/
 #RUN pip3 install mlxtend --user
-RUN pip3 install -e app/ds-colombia
 RUN pip3 install -r app/requirements.txt
+RUN pip3 install -e app/ds-colombia
 RUN pip3 install "snowflake-connector-python[pandas]"
+
+
 
 WORKDIR app
 
--- mba.py
+++ mba.py
@@ -19,10 +19,11 @@ from ds_colombia.clv.clv_churn import df_to_clv, create_transactional_table, cre
 from utils.clv_clusters_missing_products import clv_cluster_missing_products
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-
+logging.info('Loading the client json file.........\n')
 with open('clients/clients.json') as json_file:
     data = json.load(json_file)
 
+logging.info('Connecting SnowFlake.........\n')
 conn = snowflake.connector.connect(
                 user='DSCIENCE',
                 password='9VSLp#JtawhNX@UxIaAwnIH!3kwBgN&3Vwn68$M',
@@ -32,25 +33,31 @@ conn = snowflake.connector.connect(
                 schema='EXTRACAO'# ,authenticator="externalbrowser"
                 )
  
- 
-# Create a cursor object.
+logging.info('Creating a cursor object..........\n')
 cur = conn.cursor()
 
-
+logging.info('Creating a sql query..........\n')
 df_sql = f"""
-select *
-from "PBM_INTER"."TRUSTED_COMFANDI"."T_COMFANDI_SALE_TABLEAU"
-limit 10
-"""
+              select product_id, product_name, product_group,sale_unity,sale_order_id,
+                     sale_total_price,cod_dim_customer,store_name,product_atclevel4_description,
+                     customer_type,customer_group,customer_category,customer_salary,
+                     customer_city, customer_gender, customer_age, customer_family,
+                     product_brand, product_class, product_presentation, product_sub_family, product_family
+              from "PBM_INTER"."TRUSTED_COMFANDI"."T_COMFANDI_SALE_TABLEAU"
+              where year(SALE_DATE) > 2020 and sale_order_id is not null
+          """
 
+logging.info('Executing the sql query..........\n')
 cur.execute(df_sql)
 
-# Fetch the result set from the cursor and deliver it as the Pandas DataFrame.
+logging.info('Fetch the result set from the cursor and deliver it as the Pandas DataFrame..........\n')
 df = cur.fetch_pandas_all()
+df.columns = df.columns.str.lower()
 
 # Local file to test 
 #df = pd.read_parquet('data_test202211.parquet', engine='pyarrow') 
- 
+#df = pd.read_parquet('comfandi.parquet')
+
 report_date = str(datetime.now())[:10]
 bucket = 'fnc-data-science'
 products_path = f"test-colombia/products"
@@ -85,31 +92,37 @@ columns = [client_columns['product_granularity'],
 #logging.info("Reading files. Dont't mind the logging overload")
 #df = wr.s3.read_parquet(view_path, columns=columns)
 
-#logging.info("Let's run the MBA stuff")
+logging.info("\nLet's run the MBA stuff")
 allowed_product_types = list(
     pd.read_csv('clients/comfandi/comfandi_product_categories.csv')['Allowed Categories'].values)
 
-#df = df[df[client_columns['product_granularity']].isin(allowed_product_types)]
-logging.info('dataframe head - {}'.format(df.head()))
+df = df[df[client_columns['product_granularity']].isin(allowed_product_types)]
 
-#mba_generator(df,
-#              bucket,
-#              products_path,
-#              report_date,
-#              client_columns['product_granularity'],
-#              client_columns['product_name'],
-#              client_columns['product_group'],
-#              client_columns['date_column'],
-#              client_columns['quantity_column'],
-#              client_columns['order_column'],
-#              client_columns['value_column'],
-#              n_products=300,
-#              n_rules=500,
-#              min_support=0.00005,
-#              max_len=2,
-#              allowed_product_types=allowed_product_types,
-#              customer='comfandi')
 
+logging.info('Starting with the MBA function......\n')
+
+logging.info('The dataframe shape is\n')
+logging.info('Rows- {}'.format(df.shape[0]))
+logging.info('Cols- {}'.format(df.shape[1]))
+mba_generator(df,
+              bucket,
+              products_path,
+              report_date,
+              client_columns['product_granularity'],
+              client_columns['product_name'],
+              client_columns['product_group'],
+              client_columns['date_column'],
+              client_columns['quantity_column'],
+              client_columns['order_column'],
+              client_columns['value_column'],
+              n_products=300,
+              n_rules = 500,
+              min_support=0.00005,
+              max_len=2,
+              allowed_product_types=allowed_product_types,
+              customer='comfandi')
+
+logging.info("\nMBA finished without problems")
 
 #logging.info("Now it's the CLV and missing products turn")
 # end_date = date.today().strftime("%Y-%m-%d")
